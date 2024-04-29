a4af779da8e3b4143c7c6424eff3b154e208fd9b
Fael <rafinhalelograma@hotmail.com>
2024-04-29 13:48:19 -0300

    Another tests

--- src/venom.js
+++ src/venom.js
@@ -14,7 +14,7 @@ export class VenomBot {
     this.#session = session
     this.#venombot = await create({
       session,
-      headless,
+      headless:'new',
       useChrome,
       multidevice: false,
     })
